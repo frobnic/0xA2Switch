d1c7cbfbd3d23cf94360706f3aa2f2c430df5f7e
frank paulsen <frobnic+ita@gmail.com>
2019-08-26 09:16:06 +0200

    code inserted

--- 0xA2Switch/Program.cs
+++ 0xA2Switch/Program.cs
@@ -10,6 +10,31 @@ namespace _0xA2Switch
     {
         static void Main(string[] args)
         {
+            char c;
+
+            Console.Write("Bitte antworten Sie!!!");
+            c = Console.ReadKey().KeyChar;
+            c = char.ToLower(c);
+
+            Console.WriteLine();
+
+            switch (c)
+            {
+                case 'j':
+                case 'o':
+                case 'y':
+                    Console.WriteLine("Sie haben mit 'Ja' geantwortet");
+                    break;
+
+                case 'n':
+                    Console.WriteLine("Sie haben mit 'Nein' geantwortet");
+                    break;
+
+                default:
+                    Console.WriteLine("Sie haben eine falsche Eingabe gemacht");
+                    break;
+            }
+
         }
     }
 }
